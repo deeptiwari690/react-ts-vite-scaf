cf5c62ee13502242bf39605b54fd076e7564dc7c
Deepak Tiwari <deeptiwari690@gmail.com>
2026-05-20 19:20:08 +0530

    remove base

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/react-netflix-ui/",
+  // base: "/base/",
   plugins: [react()],
 });
